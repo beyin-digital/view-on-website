567bfaf551b65cb92af82572a4870119f5824690
Mohammed <salih.codes@gmail.com>
2023-06-25 23:51:11 +0300

    fix: fixing payment page reroute

--- client/pages/payment.tsx
+++ client/pages/payment.tsx
@@ -1,26 +1,29 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import Seo from "@/components/Seo";
-import { useTranslation } from "next-i18next";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Seo from '@/components/Seo'
+import { useTranslation } from 'next-i18next'
 
 const Payment = () => {
-	const { t } = useTranslation("payment");
+  const { t } = useTranslation('payment')
 
-	const router = useRouter();
+  const router = useRouter()
 
-	useEffect(() => {
-		if (router.query.hashtag) {
-			router.push(
-				`/${router.locale}/dashboard?hashtag=${decodeURI(
-					router.query.hashtag as string,
-				)}&page=${router.query.page}&limit=${router.query.limit}`,
-			);
-		}
-	}, [router.query.hashtag]);
+  useEffect(() => {
+    if (router.query.hashtag) {
+      router.push(
+        `/${router.locale}/dashboard?hashtag=${decodeURI(
+          router.query.hashtag as string
+        )}&page=${router.query.page}&limit=${router.query.limit}`
+      )
+      return
+    } else {
+      router.push('/subscribe')
+    }
+  }, [router.query.hashtag])
 
-	return <></>;
-};
+  return <></>
+}
 
-export default Payment;
+export default Payment
 
 // http://localhost:3000/payment?hashtag=p&page=1&limit=18
