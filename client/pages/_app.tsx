2734a8b27324c3ab3386d9074e0c32e2ab4d22e3
Mohammed <salih.codes@gmail.com>
2023-07-05 18:01:35 +0300

    feat:snapchat script tag

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -105,16 +105,65 @@ function MyApp(props: MyAppProps) {
                            gtag('config', 'AW-11246193982');
       `}
         </Script>
+        <script type="text/javascript">
+          {` _linkedin_partner_id = "5690257"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}{' '}
+        </script>
+        <script type="text/javascript">
+          {` (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk);`}{' '}
+        </script>
+        <noscript>
+          {' '}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=5690257&fmt=gif"
+          />{' '}
+        </noscript>
+        {/* facebook */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+								{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+								n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+								if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+								n.queue=[];t=b.createElement(e);t.async=!0;
+								t.src=v;s=b.getElementsByTagName(e)[0];
+								s.parentNode.insertBefore(t,s)}(window, document,'script',
+								'https://connect.facebook.net/en_US/fbevents.js');
+								fbq('init', '1429998061166129');
+								fbq('track', 'PageView');
+      `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+								src="https://www.facebook.com/tr?id=1429998061166129&ev=PageView&noscript=1"
+								/>`,
+          }}
+        />
+        {/* snapchat */}
+        <script type="text/javascript">
+          {`(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+						{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+						a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+						r.src=n;var u=t.getElementsByTagName(s)[0];
+						u.parentNode.insertBefore(r,u);})(window,document,
+						'https://sc-static.net/scevent.min.js');
+
+						snaptr('init', 'b63c4e3a-23d7-46d0-9e14-c3d2a152d705', {
+						'user_email': 'INSERT_USER_EMAIL'
+						});
+
+						snaptr('track', 'PAGE_VIEW');`}
+        </script>
       </Head>
 
       <div dir={isRTL ? 'rtl' : 'ltr'}>
         <Layout>
           <Component {...pageProps} />
-          {/* facebook */}
-          <Pixel name="FACEBOOK_PIXEL_1" />
-          {/* linkedin */}
-          <Linkedin />
-          {/* tiktok */}
           <TikTokPixelLoader />
         </Layout>
       </div>
