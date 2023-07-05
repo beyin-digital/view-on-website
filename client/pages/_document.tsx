01f3ae0bd8a756783fa2efc92fe56dc203c58fd9
Mohammed <salih.codes@gmail.com>
2023-07-05 16:59:43 +0300

    fix: root node_modules to gitignore

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,117 +1,117 @@
-import * as React from "react";
+import * as React from 'react'
 import Document, {
-	Html,
-	Head,
-	Main,
-	NextScript,
-	DocumentProps,
-	DocumentContext,
-} from "next/document";
-import createEmotionServer from "@emotion/server/create-instance";
-import { AppType } from "next/app";
-import theme from "../src/theme";
-import createEmotionCache from "../src/createEmotionCache";
-import { MyAppProps } from "./_app";
-import Script from "next/script";
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentProps,
+  DocumentContext,
+} from 'next/document'
+import createEmotionServer from '@emotion/server/create-instance'
+import { AppType } from 'next/app'
+import theme from '../src/theme'
+import createEmotionCache from '../src/createEmotionCache'
+import { MyAppProps } from './_app'
+import Script from 'next/script'
 
 interface MyDocumentProps extends DocumentProps {
-	emotionStyleTags: JSX.Element[];
+  emotionStyleTags: JSX.Element[]
 }
 
 export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
-	return (
-		<Html lang="en">
-			<Head>
-				{/* PWA primary color */}
-				<meta name="theme-color" content="#FFFFFF" />
-				<meta name="emotion-insertion-point" content="" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Comforter&family=Finlandica:ital,wght@1,400;1,500;1,700&family=Gentium+Book+Plus:ital,wght@0,400;0,700;1,400;1,700&family=IBM+Plex+Sans+Arabic:wght@200;300;400;500;600;700&family=Inter:wght@200;400;500;600;700;800;900&family=League+Spartan:wght@300;400;500;600&family=Neonderthaw&family=Poppins:wght@100;300;400;500;600;700;900&display=swap"
-					rel="stylesheet"
-				/>
-				<Script
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `    window.dataLayer = window.dataLayer || [];
+  return (
+    <Html lang="en">
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="emotion-insertion-point" content="" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comforter&family=Finlandica:ital,wght@1,400;1,500;1,700&family=Gentium+Book+Plus:ital,wght@0,400;0,700;1,400;1,700&family=IBM+Plex+Sans+Arabic:wght@200;300;400;500;600;700&family=Inter:wght@200;400;500;600;700;800;900&family=League+Spartan:wght@300;400;500;600&family=Neonderthaw&family=Poppins:wght@100;300;400;500;600;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `    window.dataLayer = window.dataLayer || [];
                           function gtag(){dataLayer.push(arguments);}
                           gtag('js', new Date());
                            gtag('config', 'AW-11246193982');`,
-					}}
-				></Script>
-				{emotionStyleTags}
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-				<noscript
-					dangerouslySetInnerHTML={{
-						__html: `<iframe src="https://www.googletagmanager.com/gtag/js?id=AW-11246193982"
+          }}
+        ></Script>
+        {emotionStyleTags}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/gtag/js?id=AW-11246193982"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-					}}
-				></noscript>
-			</body>
-		</Html>
-	);
+          }}
+        ></noscript>
+      </body>
+    </Html>
+  )
 }
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-	// Resolution order
-	//
-	// On the server:
-	// 1. app.getInitialProps
-	// 2. page.getInitialProps
-	// 3. document.getInitialProps
-	// 4. app.render
-	// 5. page.render
-	// 6. document.render
-	//
-	// On the server with error:
-	// 1. document.getInitialProps
-	// 2. app.render
-	// 3. page.render
-	// 4. document.render
-	//
-	// On the client
-	// 1. app.getInitialProps
-	// 2. page.getInitialProps
-	// 3. app.render
-	// 4. page.render
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
 
-	const originalRenderPage = ctx.renderPage;
+  const originalRenderPage = ctx.renderPage
 
-	// You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
-	// However, be aware that it can have global side effects.
-	const cache = createEmotionCache();
-	const { extractCriticalToChunks } = createEmotionServer(cache);
+  // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
 
-	ctx.renderPage = () =>
-		originalRenderPage({
-			enhanceApp: (
-				App: React.ComponentType<React.ComponentProps<AppType> & MyAppProps>,
-			) =>
-				function EnhanceApp(props) {
-					return <App emotionCache={cache} {...props} />;
-				},
-		});
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (
+        App: React.ComponentType<React.ComponentProps<AppType> & MyAppProps>
+      ) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />
+        },
+    })
 
-	const initialProps = await Document.getInitialProps(ctx);
-	// This is important. It prevents Emotion to render invalid HTML.
-	// See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
-	const emotionStyles = extractCriticalToChunks(initialProps.html);
-	const emotionStyleTags = emotionStyles.styles.map((style) => (
-		<style
-			data-emotion={`${style.key} ${style.ids.join(" ")}`}
-			key={style.key}
-			// eslint-disable-next-line react/no-danger
-			dangerouslySetInnerHTML={{ __html: style.css }}
-		/>
-	));
+  const initialProps = await Document.getInitialProps(ctx)
+  // This is important. It prevents Emotion to render invalid HTML.
+  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
 
-	return {
-		...initialProps,
-		emotionStyleTags,
-	};
-};
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  }
+}
